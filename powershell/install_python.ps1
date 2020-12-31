fd0aa63d8d287c57809c8f434d48f16e719e2c14
ahfx <bj.adam.harder@gmail.com>
2020-12-31 20:04:55 +0000

    added python and sublime install

---
+++ powershell/install_python.ps1
@@ -0,0 +1,40 @@
+# Silent Install Firefox 
+
+# Path for the workdir
+$workdir = "c:\installer\"
+
+# Check if work directory exists if not create it
+
+If (Test-Path -Path $workdir -PathType Container)
+{ Write-Host "$workdir already exists" -ForegroundColor Red}
+ELSE
+{ New-Item -Path $workdir  -ItemType directory }
+
+# Download the installer
+
+$source = "https://www.python.org/ftp/python/3.9.1/python-3.9.1-amd64.exe"
+$destination = "$workdir\install.exe"
+
+# Check if Invoke-Webrequest exists otherwise execute WebClient
+
+if (Get-Command 'Invoke-Webrequest')
+{
+     Invoke-WebRequest $source -OutFile $destination
+}
+else
+{
+    $WebClient = New-Object System.Net.WebClient
+    $webclient.DownloadFile($source, $destination)
+}
+
+# Start the installation
+
+Start-Process -FilePath "$workdir\install.exe" -ArgumentList "/S"
+
+# Wait XX Seconds for the installation to finish
+
+Start-Sleep -s 35
+
+# Remove the installer
+
+rm -Force $workdir\install*
---
+++ powershell/install_sublime.ps1
@@ -0,0 +1,38 @@
+# Path for the workdir
+$workdir = "c:\installer\"
+
+# Check if work directory exists if not create it
+
+If (Test-Path -Path $workdir -PathType Container)
+{ Write-Host "$workdir already exists" -ForegroundColor Red}
+ELSE
+{ New-Item -Path $workdir  -ItemType directory }
+
+# Download the installer
+
+$source = "https://download.sublimetext.com/Sublime Text Build 3211 x64 Setup.exe"
+$destination = "$workdir\install.exe"
+
+# Check if Invoke-Webrequest exists otherwise execute WebClient
+
+if (Get-Command 'Invoke-Webrequest')
+{
+     Invoke-WebRequest $source -OutFile $destination
+}
+else
+{
+    $WebClient = New-Object System.Net.WebClient
+    $webclient.DownloadFile($source, $destination)
+}
+
+# Start the installation
+
+Start-Process -FilePath "$workdir\install.exe" -ArgumentList "/S"
+
+# Wait XX Seconds for the installation to finish
+
+Start-Sleep -s 35
+
+# Remove the installer
+
+rm -Force $workdir\install
